47b07bab84396578f1868f63a6e09afcd27931c2
Renan Porto <renan.porto1099@gmail.com>
2020-09-24 11:27:36 -0300

    ensure RemoteAuthentication returns an Account on 200

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -2,6 +2,7 @@ import 'package:meta/meta.dart';
 
 import '../../domain/usecases/usecases.dart';
 import '../../domain/helpers/helpers.dart';
+import '../../domain/entities/entities.dart';
 
 import '../http/http.dart';
 
@@ -11,10 +12,11 @@ class RemoteAuthentication {
 
   RemoteAuthentication({ @required this.httpClient, @required this.url });
 
-  Future<void> auth(AuthenticationParams params) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
-      await httpClient.request(url: url, method: 'post', body: body);
+      final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
+      return AccountEntity.fromJson(httpResponse);
     } on HttpError catch(error) {
       throw error == HttpError.unauthorized
         ? DomainError.invalidCredentials
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -25,6 +25,9 @@ void main() {
   });
   
   test('Should call HttpClient with correct values', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenAnswer((_) async => {'acessToken': faker.guid.guid(), 'name': faker.person.name() });
+
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -68,5 +71,15 @@ void main() {
     final future = sut.auth(params);
     
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test('Should return an Account if HttpCLient returns 200', () async {
+    final accessToken = faker.guid.guid();
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenAnswer((_) async => {'accessToken': accessToken, 'name': faker.person.name() });
+
+    final account = await sut.auth(params);
+    
+    expect(account.token, accessToken);
   });
 }
