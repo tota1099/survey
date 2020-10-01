cbae2947199072a6086a3695669b1d565b2c7f79
Renan Porto <renan.porto1099@gmail.com>
2020-10-01 14:24:44 -0300

    ensure HttpAdapter throws BadRequest error on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:survey/data/http/http.dart';
 import 'package:survey/data/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -80,6 +81,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
