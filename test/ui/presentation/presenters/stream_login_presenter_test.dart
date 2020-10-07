dcd6c056f584c84cd1f4083e7d1b7dcb2e6184e5
Renan Porto <renan.porto1099@gmail.com>
2020-10-07 19:15:57 -0300

    ensure StreamLoginPresenter emits form valid event if form is valid

--- test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -86,4 +86,14 @@ void main() {
     sut.validateEmail(email);
     sut.validatePassword(email);
   });
+
+  test('Should emit form valid if both field is valid', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+    
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(email);
+  });
 }
